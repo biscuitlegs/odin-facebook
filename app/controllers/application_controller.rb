e6166a04c9c685f08f5b34921a44b9909188fc88
Bradley <bmcguigandev@outlook.com>
2020-11-27 11:41:31 +0000

    add posts feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :require_login, except: [:new, :create], if: :devise_controller?
 
     protected
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :first_name, :last_name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name])
+    end
+
+    def require_login
+        redirect_to unauthenticated_root_path unless user_signed_in?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   devise_scope :user do
     authenticated :user do
       root to: "posts#index", as: :authenticated_root
     end
     unauthenticated :user do
-      root to: "devise/sessions#new", as: :unauthenticated_root
+      root to: "devise/registrations#new", as: :unauthenticated_root
     end
   end
 end
