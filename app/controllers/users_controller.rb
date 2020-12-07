f39bd539c3548956ee849b8967fb5995b3e9db05
Bradley <bmcguigandev@outlook.com>
2020-12-07 02:30:38 +0000

    fix bug with users search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def index
         if params[:search]
-            @users = User.where("LOWER(CONCAT(first_name, last_name)) LIKE LOWER(?)", "%#{params[:search]}%")
+            @users = User.where("LOWER(CONCAT(first_name, ' ', last_name)) LIKE LOWER(?)", "%#{params[:search]}%")
         else
             @users = User.where("id != ?", current_user.id)
         end
